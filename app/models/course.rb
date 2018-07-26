430a57fec2bb2135c2db8e718b95156a717e9e46
Andy Smith <andjsmit@iupui.edu>
2018-07-26 16:23:52 -0400

    Creates course model and associates items to it.

---
+++ app/models/course.rb
@@ -0,0 +1,5 @@
+class Course < ApplicationRecord
+
+  has_many :items, dependent: :destroy
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725164925) do
+ActiveRecord::Schema.define(version: 20180726201706) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.string "course_number"
+    t.string "instructor"
+    t.string "instructor_username"
+    t.string "library"
+    t.string "semester"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -26,6 +37,8 @@ ActiveRecord::Schema.define(version: 20180725164925) do
     t.integer "iucat_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "course_id"
+    t.index ["course_id"], name: "index_items_on_course_id"
   end
 
 end
