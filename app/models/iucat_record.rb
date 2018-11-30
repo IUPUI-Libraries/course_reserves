329936a3e359a0e78334da28cac21d08617fc6e8
Andy Smith <andjsmit@iupui.edu>
2018-11-30 10:49:13 -0500

    Clean up titles and allow record url as well as id in iucat parse.

--- app/models/iucat_record.rb
+++ app/models/iucat_record.rb
@@ -2,7 +2,12 @@ class IucatRecord
   require 'open-uri'
 
   def initialize(record_string)
-    @record = Nokogiri::HTML(open("https://iucat.iu.edu/catalog/#{record_string}/librarian_view")).css('div#marc_view div.field')
+    unless record_string.include? "http"
+      record_string = "https://iucat.iu.edu/catalog/#{record_string}/librarian_view"
+    else
+      record_string += '/librarian_view'
+    end
+    @record = Nokogiri::HTML(open(record_string)).css('div#marc_view div.field')
   end
 
   def data
@@ -27,16 +32,15 @@ class IucatRecord
     tag_id = tag.css('div.tag_ind span.tag').text.delete(" \n")
     subfields = tag.css('div.subfields')
     subfields_split = subfields.inner_html.split(/\<span class="sub_code">.*\<\/span\>/)
-    # puts "--------------"
-    # puts subfields_split
-    # puts "--------------"
     subfield_codes = subfields.css('span.sub_code')
     i = 1
     subfield_codes.each do |subfield_code|
       sub_code = subfield_code.text.delete("|").strip
       subtag_id = "#{tag_id}#{sub_code}"
-      data[subtag_id] = subfields_split[i].delete("\n").strip
-      i = i + 1
+      sub_val = subfields_split[i].delete("\n").strip
+      sub_val.chop!.strip! if sub_val.end_with?('/')
+      data[subtag_id] = sub_val
+      i += 1
     end
     return data
   end
