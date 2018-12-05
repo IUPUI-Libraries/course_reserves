48dd1225584f804c1e2fb68d4c8d43dedd3f251f
Andy Smith <andjsmit@iupui.edu>
2018-12-05 11:36:18 -0500

    Adds roles to application.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181029190720) do
+ActiveRecord::Schema.define(version: 20181205161716) do
+
+  create_table "assignments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_assignments_on_role_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -39,6 +48,12 @@ ActiveRecord::Schema.define(version: 20181029190720) do
     t.datetime "updated_at", null: false
     t.integer "course_id"
     t.index ["course_id"], name: "index_items_on_course_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
