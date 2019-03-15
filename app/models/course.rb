df4f050d13ef41ac7d9bc704e20ca7ee2a3f5e46
Andy Smith <andjsmit@iupui.edu>
2019-03-15 14:45:03 -0400

    Prevents item from being saved/updated if title & author & publication date are empty.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,13 @@
 class Course < ApplicationRecord
   has_many :items, dependent: :destroy
   belongs_to :semester
-  accepts_nested_attributes_for :items, allow_destroy: true
+  accepts_nested_attributes_for :items,
+    allow_destroy: true,
+    reject_if: :reject_item
+
+  def reject_item(attributes)
+    attributes['title'].blank? &&
+    attributes['author'].blank? &&
+    attributes['publication_date'].blank?
+  end
 end
