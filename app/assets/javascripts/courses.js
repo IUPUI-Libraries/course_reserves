85431a9b27311208ec31519bbb33593acb2b02d4
Andy Smith <andjsmit@iupui.edu>
2019-04-02 09:19:37 -0400

    Requires confirmation before leaving course form with unsaved changes.

---
+++ app/assets/javascripts/courses.js
@@ -0,0 +1,36 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+
+// Script to prevent leaving form without saving.
+// Source: https://blog.driftingruby.com/form-check-modified/
+
+var msg, unsaved;
+
+msg = "Changes you made may not be saved.";
+
+unsaved = false;
+
+$(document).on('change', 'form[role="check-modified"]:not([data-remote]) :input', function() {
+  return unsaved = true;
+});
+
+$(document).on('turbolinks:load', function() {
+  return unsaved = false;
+});
+
+$(document).on('submit', 'form[role="check-modified"]', function() {
+  unsaved = false;
+});
+
+$(window).bind('beforeunload', function() {
+  if (unsaved) {
+    return msg;
+  }
+});
+
+$(document).on('turbolinks:before-visit', function(event) {
+  if (unsaved && !confirm(msg)) {
+    return event.preventDefault();
+  }
+});
